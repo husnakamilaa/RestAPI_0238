e88dc36d80fa763d3327154d8b11dd8ddb18006e
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-02 01:14:32 +0700

    feat: method untuk get dan validasi token

--- lib/data/repositories/hewan_repository.dart
+++ lib/data/repositories/hewan_repository.dart
@@ -7,5 +7,22 @@ class HewanRepository {
   final String baseUrl = "https://ternak-be-production.up.railway.app/api/v1";
   final StorageProvider storage = StorageProvider();
 
+  Future<List<HewanModel>> getAllHewan() async {
+    final token = await storage.getToken();
+
+    final response = await http.get(
+      Uri.parse('$baseUrl/hewan'),
+      headers: {'Authorization': 'Bearer $token', 'Accept': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> body = jsonDecode(response.body);
+      final List<dynamic> data = body['data'];
+      return data.map((item) => HewanModel.fromJson(item)).toList();
+    } else {
+      throw Exception("Gagal mengambil data hewan");
+    }
+  }
+
   
 }
