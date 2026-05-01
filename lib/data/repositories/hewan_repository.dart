f14c0ed0ed4e5ab677ad9d48bd5418c412675e4a
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-02 01:16:34 +0700

    feat: method untuk delete

--- lib/data/repositories/hewan_repository.dart
+++ lib/data/repositories/hewan_repository.dart
@@ -62,5 +62,17 @@ class HewanRepository {
     }
   }
 
-  
+  Future<void> deleteHewan(int id) async {
+    final token = await storage.getToken();
+
+    final response = await http.delete(
+      Uri.parse('$baseUrl/hewan/$id'),
+      headers: {'Accept': 'application/json', 'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode != 200 && response.statusCode != 204) {
+      final data = jsonDecode(response.body);
+      throw data['message'] ?? 'Gagal menghapus data hewan';
+    }
+  }
 }
