9cd83d836930de278831bbb4b5cc37bdbc6b84c9
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-02 00:25:12 +0700

    feat: bikin method untuk ambil dan hapus token stlh digunakan

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -8,5 +8,17 @@ class AuthRepository {
   final String baseUrl = "https://ternak-be-production.up.railway.app/api/v1";
   final _storage = const FlutterSecureStorage();
 
+  Future<void> persistToken(String token) async {
+    await _storage.write(key: 'jwt_token', value: token);
+  }
+
+  Future<String?> getToken() async {
+    return await _storage.read(key: 'jwt_token');
+  }
+
+  Future<void> deleteToken() async {
+    await _storage.delete(key: 'jwt_token');
+  }
+
   
 }
