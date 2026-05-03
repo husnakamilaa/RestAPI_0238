95614f581a75c5eb1286f3424b85d4936d604858
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-03 18:14:17 +0700

    fix: benerin bagian main dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rest_api/logic/bloc/auth/auth_bloc.dart';
 import 'package:rest_api/logic/bloc/auth/auth_event.dart';
 import 'package:rest_api/data/repositories/auth_repository.dart';
+import 'package:rest_api/logic/bloc/auth/auth_state.dart';
 import 'package:rest_api/logic/ui/pages/dashboard_page.dart';
 import 'package:rest_api/logic/ui/pages/login_page.dart';
 import 'package:rest_api/logic/ui/pages/register_page.dart';
@@ -18,23 +19,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => AuthBloc(
-        repository: AuthRepository(),
-      )..add(AppStarted()), 
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          useMaterial3: true,
-          colorSchemeSeed: Colors.indigo,
-        ),
-
-        initialRoute: '/login',
-        routes: {
-          '/login': (context) => const LoginPage(),
-          '/register': (context) => const RegisterPage(),
-          '/dashboard': (context) => const DashboardPage(),
+      create: (_) => AuthBloc(repository: AuthRepository())..add(AppStarted()),
+      child: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              useMaterial3: true,
+              colorSchemeSeed: Colors.indigo,
+            ),
+            title: "Aplikasi Ternak",
+            home: _getHome(state),
+          );
         },
       ),
     );
   }
+
+  
 }
