3751bb94289c8da874615267e25dbdf77aa9e2d9
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-03 17:38:11 +0700

    feat: dekarasi addhewan page sama variable _formkey buat validasi

---
+++ lib/logic/ui/pages/add_hewan_page.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rest_api/logic/bloc/hewan/hewan_bloc.dart';
+import 'package:rest_api/logic/bloc/hewan/hewan_event.dart';
+
+class AddHewanPage extends StatefulWidget {
+  const AddHewanPage({super.key});
+
+  @override
+  State<AddHewanPage> createState() => _AddHewanPageState();
+}
+
+class _AddHewanPageState extends State<AddHewanPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  
+}
